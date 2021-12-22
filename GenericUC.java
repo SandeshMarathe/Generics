491a5ef79b134f5ff805173b1dd35c8c47816b70
SandeshMarathe <marathesandesh1999@gmail.com>
2021-12-22 11:38:26 +0530

    ADDED: Generic Program to Find 3 Float Maximum

--- GenericUC.java
+++ GenericUC.java
@@ -11,9 +11,22 @@ public class GenericUC {
 		return max;
 	}
 
+	public static float checkMaximum(Float d, Float e, Float f) {
+		Float max = d;
+		if(e.compareTo(max) > 0) {
+			max = e;
+		}
+		if(f.compareTo(max) > 0) {
+			max = f;
+		}
+		return max;
+	}
+
 	public static void main(String[] args) {
 		int c = checkMaximum(10,23,15);
+		float d = checkMaximum(12.5f, 13.4f, 22.0f);
 		System.out.println(c);
+		System.out.println(d);
 	}
 
 }
