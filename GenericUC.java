19325b6553482655172cfbf4ccc5a5492967c0f2
SandeshMarathe <marathesandesh1999@gmail.com>
2021-12-22 11:24:14 +0530

    ADDED : Generic Program to Find 3 Integer Maximum

---
+++ GenericUC.java
@@ -0,0 +1,19 @@
+public class GenericUC {
+
+	public static int checkMaximum(Integer x, Integer y, Integer z) {
+		Integer max = x;
+		if(y.compareTo(max) > 0) {
+			max = y;
+		}
+		if(z.compareTo(max) > 0) {
+			max = z;
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+		int c = checkMaximum(10,23,15);
+		System.out.println(c);
+	}
+
+}
